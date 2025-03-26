68c7797e499a4285d9962531d04264bcd68ebf9c
5t3x <kissmatemail@gmail.com>
2025-03-26 10:54:38 +0100

    jatekelem reset

--- JatekElem.js
+++ JatekElem.js
@@ -2,25 +2,9 @@ export default class JatekElem {
     constructor(index, initialState) {
         this.index = index;
         this.allapot = kezdetiAllapot;
-        this.elem = document.createElement("div");
         this.elem.classList.add("lampa");
         this.frissitSzint();
         this.elem.addEventListener("click", () => this.kattintas());
     }
 
-    frissitSzint() {
-        this.elem.style.backgroundColor = this.allapot ? "yellow" : "gray";
-    }
-
-    kattintas() {
-        const esemeny = new CustomEvent("allapotValtozas", {
-            detail: { index: this.index }
-        });
-        window.dispatchEvent(esemeny);
-    }
-
-    allapotValtas() {
-        this.allapot = !this.allapot;
-        this.frissitSzint();
-    }
 }
