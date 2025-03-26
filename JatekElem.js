4ef8f6a06e1cd1b8af05e170a2a948969850ac39
5t3x <kissmatemail@gmail.com>
2025-03-26 10:35:53 +0100

    jatekelem alap

---
+++ JatekElem.js
@@ -0,0 +1,26 @@
+class Lampa {
+    constructor(index, initialState) {
+        this.index = index; // Pozíció a játéktérben
+        this.allapot = kezdetiAllapot; // Kezdeti állapot
+        this.elem = document.createElement("div");
+        this.elem.classList.add("lampa");
+        this.frissitSzint(); // Háttérszín frissítése
+        this.elem.addEventListener("click", () => this.kattintas());
+    }
+
+    frissitSzint() {
+        this.elem.style.backgroundColor = this.allapot ? "yellow" : "gray";
+    }
+
+    kattintas() {
+        const esemeny = new CustomEvent("allapotValtozas", {
+            detail: { index: this.index }
+        });
+        window.dispatchEvent(esemeny); // Esemény küldése
+    }
+
+    allapotValtas() {
+        this.allapot = !this.allapot;
+        this.frissitSzint(); // Szín frissítése állapot alapján
+    }
+}
