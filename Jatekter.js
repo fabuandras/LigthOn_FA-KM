7b7c4bad6a6576aad57f90351e32699dd237ecfe
fabuandras <fabuandras@gmail.com>
2025-03-26 11:19:34 +0100

    Jatekter.js modositasa

--- Jatekter.js
+++ Jatekter.js
@@ -1,21 +1,31 @@
 import JatekElem from "./JatekElem.js";
 
 export default class JatekTer {
-    #lista=[" ", "", " ", " ", " ", " ", " ", " ", ""];
+    #lista = [false, true, false, false, false, false, false, false, true];
+    //false: le van kapcsolva a lámpa
+    //true: fel van kapcsolva a lámpa
     constructor(szuloElem) {
         this.szuloElem = szuloElem;
         this.megjelenit();
         this.kivalasztEsemenykezelo();
     }
 
+    kivalasztEsemenykezelo() {
+        window.addEventListener("kivalaszt", (event) => {
+            let index = event.detail;
+            this.#lista[index] = !this.#lista[index];
+            this.megjelenit();
+        });
+    }
+
     megjelenit() {
         this.szuloElem.innerHTML = "";
-        this.#lista.forEach((allapot, index) => {
+        for (let index = 0; index < this.#lista.length; index++) {
             new JatekElem(
-                allapot,
+                this.#lista[index],
                 index,
                 this.szuloElem
             );
-        });
+        }
     }
 }
