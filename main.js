cca7963114610ecd1474058afdf61cc798a5123b
fabuandras <fabuandras@gmail.com>
2025-03-26 11:57:11 +0100

    kesz - style nelkul

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Jatekter from "./Jatekter.js";
-import InfoBox from "./InfoBox.js";
+//import InfoBox from "./InfoBox.js";
 
 const jatekterElem = document.querySelector(".jatekter");
 const jatekter = new Jatekter(jatekterElem);
